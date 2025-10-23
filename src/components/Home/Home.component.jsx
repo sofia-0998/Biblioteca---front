6fa054c6e40102aa3a1515101aa31e1a24bc275b
Khazdrags <lautaromarzo1998@gmail.com>
2025-10-23 16:05:42 -0300

    feat: Agregar fondo de pantalla y mejorar la interfaz de inicio de sesión y página de inicio

--- src/components/Home/Home.component.jsx
+++ src/components/Home/Home.component.jsx
@@ -1,11 +1,114 @@
-import React from 'react';
+import { Typography, Box, Button } from "@mui/material";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Users, BookOpenText, LibraryBig } from "lucide-react";
 
 const Home = () => {
-return(
-    <div>
-        Home 222222222222222222222222222
-    </div>
-)
-}
+  const navigate = useNavigate();
 
-export default Home
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        minHeight: "80vh",
+        gap: 4,
+      }}
+    >
+      <Typography variant="h3" sx={{ mb: 4 }} color="primary">
+        Bienvenido a Biblioteca A5
+      </Typography>
+
+      <Box
+        sx={{
+          display: "flex",
+          gap: 4,
+          flexWrap: "wrap",
+          justifyContent: "center",
+        }}
+      >
+        <Button
+          onClick={() => navigate("/clientes")}
+          sx={{
+            width: 200,
+            height: 200,
+            borderRadius: 5,
+            boxShadow: 3,
+            display: "flex",
+            flexDirection: "column",
+            gap: 2,
+            backgroundColor: "primary.light",
+            color: "black",
+            transition: "all 0.3s ease",
+            "&:hover": {
+              backgroundColor: "primary.main",
+              boxShadow: 6,
+              transform: "scale(1.05)",
+            },
+          }}
+        >
+          <Users size={60} color="#f5f5f5" />
+          <Typography variant="h5" color="#f5f5f5">
+            Clientes
+          </Typography>
+        </Button>
+
+        <Button
+          onClick={() => navigate("/libros")}
+          sx={{
+            width: 200,
+            height: 200,
+            borderRadius: 5,
+            boxShadow: 3,
+            display: "flex",
+            flexDirection: "column",
+            gap: 2,
+            backgroundColor: "primary.light",
+            color: "black",
+            transition: "all 0.3s ease",
+            "&:hover": {
+              backgroundColor: "primary.main",
+              boxShadow: 6,
+              transform: "scale(1.05)",
+            },
+          }}
+        >
+          <BookOpenText size={60} color="#f5f5f5" />
+          <Typography variant="h5" color="#f5f5f5">
+            Libros
+          </Typography>
+        </Button>
+
+        <Button
+          onClick={() => navigate("/alquiler")}
+          sx={{
+            width: 200,
+            height: 200,
+            borderRadius: 5,
+            boxShadow: 3,
+            display: "flex",
+            flexDirection: "column",
+            gap: 2,
+            backgroundColor: "primary.light",
+            color: "black",
+            transition: "all 0.3s ease",
+            "&:hover": {
+              backgroundColor: "primary.main",
+              boxShadow: 6,
+              transform: "scale(1.05)",
+            },
+          }}
+        >
+          <LibraryBig size={60} color="#f5f5f5" />
+          <Typography variant="h5" color="#f5f5f5">
+            Alquiler
+          </Typography>
+        </Button>
+      </Box>
+    </Box>
+  );
+};
+
+export default Home;
--- src/components/Login/Login.component.jsx
+++ src/components/Login/Login.component.jsx
@@ -1,95 +1,144 @@
-//make simple login html only skeleton html
-import React from 'react';
-import FondoPantalla from '../Login/library-interior-empty-room-reading-with-books-wooden-shelves_33099-1722.jpg'
-import { Button, Typography, Dialog, DialogActions, DialogTitle, DialogContent, TextField } from "@mui/material";
+import React, { useState } from "react";
+import FondoPantalla from "../../assets/fondoLogin.jpg";
+import {
+  Button,
+  Typography,
+  Dialog,
+  DialogActions,
+  DialogTitle,
+  DialogContent,
+  TextField,
+} from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-    return (
-        <div className="login-container" style={{
-                backgroundImage: `url(${FondoPantalla})`,
-                backgroundSize: 'cover',
-                backgroundPosition: 'top',
-                minHeight: '100vh', // Si quieres que cubra la pantalla
-                width: '100vw',
-                paddingTop: '50px'
-            }}>
+  const [usuario, setUsuario] = useState("");
+  const [contraseña, setContraseña] = useState("");
+  const navigate = useNavigate();
 
-        <div style={{
-                backgroundColor: 'rgba(255, 255, 255, 0.75)',
-                width: '400px',
-                height: '600px', 
-                borderRadius: '10px', // Opcional: para bordes redondeados
-                marginLeft: '80px'
-            }}>
+  const handleLogin = () => {
+    console.log("Usuario:", usuario);
+    console.log("Contraseña:", contraseña);
+    navigate("/home");
+  };
 
-        <Typography style={{
-            fontSize: '2.5em', 
-            fontWeight: 'bold', 
-            fontFamily: 'Roboto, sans-serif', // Usa una fuente disponible o importada
-            color: 'black',
-            textAlign: 'center'
-        }}>
-            Biblioteca A5
+  return (
+    <div
+      className="login-container"
+      style={{
+        backgroundImage: `url(${FondoPantalla})`,
+        backgroundSize: "cover",
+        backgroundPosition: "top",
+        minHeight: "100vh", // Si quieres que cubra la pantalla
+        width: "100vw",
+        paddingTop: "50px",
+      }}
+    >
+      <div
+        style={{
+          backgroundColor: "rgba(255, 255, 255, 0.75)",
+          width: "400px",
+          height: "600px",
+          borderRadius: "10px", // Opcional: para bordes redondeados
+          marginLeft: "80px",
+        }}
+      >
+        <Typography
+          style={{
+            fontSize: "2.5em",
+            fontWeight: "bold",
+            fontFamily: "Roboto, sans-serif", // Usa una fuente disponible o importada
+            color: "black",
+            textAlign: "center",
+          }}
+        >
+          Biblioteca A5
         </Typography>
-        <Typography  style={{
-            fontSize: '1.5em', 
-            fontWeight: 'bold', 
-            fontFamily: 'Roboto, sans-serif', // Usa una fuente disponible o importada
-            color: 'black',
-            textAlign: 'center'
-        }}>
-            Iniciar Sesion
+        <Typography
+          style={{
+            fontSize: "1.5em",
+            fontWeight: "bold",
+            fontFamily: "Roboto, sans-serif", // Usa una fuente disponible o importada
+            color: "black",
+            textAlign: "center",
+          }}
+        >
+          Iniciar Sesion
         </Typography>
         <form className="login-form">
-        <div className="form-group">
-            <label htmlFor="username" style= {{
-                display: 'block', 
-                textAlign: 'center',
-                marginBottom: '5px',
-                fontWeight: 'bold'}}>
-            Usuario
+          <div className="form-group">
+            <label
+              htmlFor="username"
+              style={{
+                display: "block",
+                textAlign: "center",
+                marginBottom: "5px",
+                fontWeight: "bold",
+              }}
+            >
+              Usuario
             </label>
-            <input type="text" id="username" name="Usuario" required style={{
-                display: 'block', // Crucial para que margin: auto funcione
-                width: '80%',     // Define el ancho para que haya espacio para centrar
-                margin: '0 auto', // Centra la caja horizontalmente
-                border: '1px solid black', // Ejemplo de borde
-                borderRadius: '5px',        // Ejemplo de borde redondeado
-                padding: '10px'             // Sugerencia para mejorar el aspecto
-            }}/>
-        </div>
-        <div className="form-group">
-            <label htmlFor="password" style= {{
-                display: 'block', 
-                textAlign: 'center',
-                marginBottom: '5px',
-                fontWeight: 'bold'}}>
-            Contraseña
+            <input
+              type="text"
+              id="username"
+              name="Usuario"
+              required
+              style={{
+                display: "block", // Crucial para que margin: auto funcione
+                width: "80%", // Define el ancho para que haya espacio para centrar
+                margin: "0 auto", // Centra la caja horizontalmente
+                border: "1px solid black", // Ejemplo de borde
+                borderRadius: "5px", // Ejemplo de borde redondeado
+                padding: "10px", // Sugerencia para mejorar el aspecto
+              }}
+              value={usuario}
+              onChange={(e) => setUsuario(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label
+              htmlFor="password"
+              style={{
+                display: "block",
+                textAlign: "center",
+                marginBottom: "5px",
+                fontWeight: "bold",
+              }}
+            >
+              Contraseña
             </label>
-            <input type="password" id="password" name="Contraseña" required style={{
-                display: 'block', // Crucial para que margin: auto funcione
-                width: '80%',     // Define el ancho para que haya espacio para centrar
-                margin: '0 auto', // Centra la caja horizontalmente
-                border: '1px solid black', // Ejemplo de borde
-                borderRadius: '5px',        // Ejemplo de borde redondeado
-                padding: '10px'             // Sugerencia para mejorar el aspecto
-            }}/>
-        </div>
-        <Button variant="contained" style={{
-            marginTop: '20px',
-            display: 'block',
-            margin: '20px auto 0 auto',
-            width: 'auto'
-        }}>
-        Iniciar sesion
-        </Button>
+            <input
+              type="password"
+              id="password"
+              name="Contraseña"
+              required
+              style={{
+                display: "block", // Crucial para que margin: auto funcione
+                width: "80%", // Define el ancho para que haya espacio para centrar
+                margin: "0 auto", // Centra la caja horizontalmente
+                border: "1px solid black", // Ejemplo de borde
+                borderRadius: "5px", // Ejemplo de borde redondeado
+                padding: "10px", // Sugerencia para mejorar el aspecto
+              }}
+              value={contraseña}
+              onChange={(e) => setContraseña(e.target.value)}
+            />
+          </div>
+          <Button
+            variant="contained"
+            style={{
+              marginTop: "20px",
+              display: "block",
+              margin: "20px auto 0 auto",
+              width: "auto",
+            }}
+            onClick={handleLogin}
+          >
+            Iniciar sesion
+          </Button>
         </form>
-        </div>
-            
+      </div>
     </div>
-    );
-
+  );
 };
 export default Login;
-
-
